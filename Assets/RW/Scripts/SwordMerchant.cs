323ccda36100c3243a4d5d405d9f138279ccbbaf
dr-iskandar <drachmat.iskandar@gmail.com>
2020-03-11 12:42:25 +0700

    Initial commit

---
+++ Assets/RW/Scripts/SwordMerchant.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class SwordMerchant : MonoBehaviour
+{
+    [SerializeField]
+    private Text swordName;
+    [SerializeField]
+    private Text description;
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private Text goldCost;
+    [SerializeField]
+    private Text atackDamage;
+
+    public void UpdateDisplayUI(SwordData swordData)
+    {
+        swordName.text = swordData.SwordName;
+        description.text = swordData.Description;
+        icon.sprite = swordData.Icon;
+        goldCost.text = swordData.GoldCost.ToString();
+        atackDamage.text = swordData.AttackDamage.ToString();
+    }
+}
